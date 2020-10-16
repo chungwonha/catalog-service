fcd15ccd95a231c8d22af1e74b348554e78455f4
chungha@yahoo.com <chungha@yahoo.com>
2020-10-15 23:43:05 -0400

    simple change

--- src/main/java/com/chung/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/chung/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -9,6 +9,7 @@ public class CatalogServiceApplication {
 	//test
 	public static void main(String[] args) {
 		System.out.println("main");
+		System.out.println("main1234");
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
 
